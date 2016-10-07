bc126160b03dfcb21ec0a61ee0822c630313b55a
Mikhail Sergeev <msergeev06@gmail.com>
2016-10-07 18:21:01 +0300

    Weekly Update 7.10.16

--- msergeev/core/lib/table_helper.php
+++ msergeev/core/lib/table_helper.php
@@ -6,21 +6,52 @@ use \MSergeev\Core\Entity;
 
 class TableHelper
 {
-	public static function activeField()
+	public static function activeField($arParams=array())
 	{
-		return new Entity\BooleanField('ACTIVE',array(
+		$arResult = array(
 			'required' => true,
 			'default_value' => true,
 			'title' => 'Активность'
-		));
+		);
+		$arResult = self::parseParams($arResult,$arParams);
+
+		return new Entity\BooleanField('ACTIVE',$arResult);
 	}
 
-	public static function sortField()
+	public static function sortField($arParams=array())
 	{
-		return new Entity\IntegerField('SORT',array(
+		$arResult = array(
 			'required' => true,
 			'default_value' => 500,
 			'title' => 'Сортировка'
-		));
+		);
+		$arResult = self::parseParams($arResult,$arParams);
+
+		return new Entity\IntegerField('SORT',$arResult);
+	}
+
+	private static function parseParams (array $arResult,array $arParams)
+	{
+		if (isset($arParams['required']))
+		{
+			$arResult['required'] = $arParams['required'];
+			unset($arParams['required']);
+		}
+		if (isset($arParams['default_value']))
+		{
+			$arResult['default_value'] = $arParams['default_value'];
+			unset($arParams['default_value']);
+		}
+		if (isset($arParams['title']))
+		{
+			$arResult['title'] = $arParams['title'];
+			unset($arParams['title']);
+		}
+		if (!empty($arParams))
+		{
+			$arResult = array_merge($arResult,$arParams);
+		}
+
+		return $arResult;
 	}
 }
---
+++ msergeev/packages/owm/default_options.php
@@ -0,0 +1,5 @@
+<?php
+
+$arPackageDefaultOptions = array(
+	'OWM_APP_ID' => 'ee04dde0551874138ce0843f64e1ed19'
+);
